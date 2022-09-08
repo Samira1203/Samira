637a9a533473acfa5bb60f3e3097f083fb5d9b1f
Samira1203 <113084677+Samira1203@users.noreply.github.com>
2022-09-08 00:46:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const yes8tn=document.querySelector("#yes8tn");
+
+yes8tn.addEventListener("click",function(){alert("Samira mi amorcito simpre ganara punto final")});
+
+const no8tn =document.querySelector("#no8tn");
+
+no8tn.addEventListener("mouseover",function(){
+    const randomX = parseInt(Math.random()*50);
+    const randomY = parseInt(Math.random()*50);
+    no8tn.style.setProperty("top",randomY+"%");
+    no8tn.style.setProperty("left",randomX+"%")
+    no8tn.style.setProperty("transform","translate(-${randomX}%,-${randoY}%)");
+})
+
